04b22365cb697f123fb0887bd8756d7efd956be6
Cedurikeu <115144016+cedurikeu@users.noreply.github.com>
2025-08-14 20:58:31 +0800

    Delete Smart Building GUI v2 directory

--- Smart Building GUI v2/backend/models/UserModel.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const UserSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-});
-
-export const User = mongoose.model("User", UserSchema);
